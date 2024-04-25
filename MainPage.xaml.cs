85b99444984727904f364b834ba54183be3579e8
Николай Ермаков <nikolaiwitch2018@gmail.com>
2024-04-25 16:41:29 +0600

    Added DigitsSumFUnction

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -36,6 +36,23 @@ namespace MauiApp3
         {
             return number % TableSize;
         }
+        private int DubleHash(int number)
+        { 
+            return(DigitsSum(number) + 17)% TableSize;
+        }
+        private int DigitsSum(int number) 
+        {
+            int DigitsCount = (int)Math.Log10(number) + 1;
+            int[] digits = new int[DigitsCount];
+            int temp = number;
+            for (int i = 0; i < DigitsCount; i++)
+            {
+                int digit = temp % 10;
+                digits[i] = digit;
+                temp = (temp - digit) / 10;
+            }
+            return digits.Sum();
+        }
 
         private void ButtonAddToTable_Clicked(object sender, EventArgs e)
         {
@@ -43,17 +60,21 @@ namespace MauiApp3
             {
                 //событие на случай ошибки, то есть запарсить не удалось
             }
-
+            
             int hash = GetHash(input);
-
-            if (!HashTable.TryAdd(hash, input.ToString()))
+            if(!HashTable.TryAdd(hash, input.ToString()))
+            while (!HashTable.TryAdd(hash, input.ToString()))
             { 
-                /*
+                input = DubleHash(input);
+            }
+            /*if (!HashTable.TryAdd(hash, input.ToString()))
+            { 
+                *//*
                  TryAdd возвращает логическое значение поэтому можно проверить добавлять или нет
                 Например, если уже есть значение, то повторное хеширование
                 Или вообще это можно добавить в while
-                 */
-            }
+                 *//*
+            }*/
         }
     }
 
